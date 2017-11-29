4b9eef6092a0c752b5d275416d04f6f8a13ce682
Meneses0114 <Meneses0114@users.noreply.github.com>
2017-11-29 14:53:16 -0600

    1Transportistas Oscar

---
+++ src/Modelo/transportistas.java
@@ -0,0 +1,35 @@
+
+package Modelo;
+
+public class transportistas {
+    private String nit;
+    private String nombre;
+    private String telefono;
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+}
